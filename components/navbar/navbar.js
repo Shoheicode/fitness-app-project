1fda9efddca22d421e13bcf2c8a8c0a844415dbb
Ian Dai <ianbbdai@gmail.com>
2024-09-08 02:46:16 -0700

    Created streak icon

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,148 +1,111 @@
-import { SignedIn, SignedOut, SignIn, UserButton, useUser } from "@clerk/nextjs"
-import { AppBar, Box, Button, createTheme, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, ThemeProvider, Toolbar, Typography } from "@mui/material"
-import Link from "next/link"
-import * as React from 'react';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from '@mui/icons-material/Home';
-import CreateIcon from '@mui/icons-material/Create';
-import SearchIcon from '@mui/icons-material/Search';
-import SortIcon from '@mui/icons-material/Sort';
-import FolderIcon from '@mui/icons-material/Folder';
-import { Folder, Help, Image } from "@mui/icons-material";
-import HelpIcon from '@mui/icons-material/Help';
-import RateReviewIcon from '@mui/icons-material/RateReview';
-import SmartToyIcon from '@mui/icons-material/SmartToy';
-import '@/app/CSS/LandingPage.css'
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import {
+  AppBar,
+  Box,
+  Button,
+  createTheme,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Link from "next/link";
+import * as React from "react";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import CreateIcon from "@mui/icons-material/Create";
+import SearchIcon from "@mui/icons-material/Search";
+import SortIcon from "@mui/icons-material/Sort";
+import FolderIcon from "@mui/icons-material/Folder";
+import HelpIcon from "@mui/icons-material/Help";
+import RateReviewIcon from "@mui/icons-material/RateReview";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+import "@/app/CSS/LandingPage.css";
+import FlameIcon from "../flameIcon/flameIcon";
 
-export default function NavBar(){
-
+export default function NavBar() {
   const darkTheme = createTheme({
     palette: {
-      mode: 'dark',
+      mode: "dark",
       primary: {
-        main: '#1976d2',
+        main: "#1976d2",
       },
     },
   });
 
-    const [open, setOpen] = React.useState(false);
-    
-    const toggleDrawer = (newOpen) => () => {
-      setOpen(newOpen);
-    };
-    
-    const { isLoaded, isSignedIn, user } = useUser();
+  const [open, setOpen] = React.useState(false);
 
-    const [userName, setUsername] = React.useState("");
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
 
+  const { isLoaded, isSignedIn, user } = useUser();
 
-    const DrawerList = (
-      <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-        <List>
+  const [userName, setUsername] = React.useState("");
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
         <ListItem key={"AStar Fitness"} disablePadding>
-            <ListItemButton href="/">
-              <ListItemIcon>
-                <HomeIcon></HomeIcon>
-              </ListItemIcon>
-              <Typography
-                className="apply"  
-              >
-                AStar Fitness
-              </Typography> 
-            </ListItemButton>
-          </ListItem>
-          <ListItem key={"Fitness Bot"} disablePadding>
-            <ListItemButton href="/FitnessBot">
-              <ListItemIcon>
-                <SmartToyIcon></SmartToyIcon>
-              </ListItemIcon>
-              <Typography
-                className="apply"  
-              >
-                Fitness Bot
-              </Typography> 
-            </ListItemButton>
-          </ListItem>
-        </List>
-        <Divider />
-        <Stack
-          width={"100%"}
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          gap={4}
-          padding={4}
-        >
-          <SignedIn>
-            
-            <UserButton />
-          </SignedIn>
-          <Typography
-            fontSize={20}
-          >
-            {isLoaded && isSignedIn && user.fullName}
-          </Typography>
-        </Stack>
-      </Box>
-    );
+          <ListItemButton href="/">
+            <ListItemIcon>
+              <HomeIcon></HomeIcon>
+            </ListItemIcon>
+            <Typography className="apply">AStar Fitness</Typography>
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={"Fitness Bot"} disablePadding>
+          <ListItemButton href="/FitnessBot">
+            <ListItemIcon>
+              <SmartToyIcon></SmartToyIcon>
+            </ListItemIcon>
+            <Typography className="apply">Fitness Bot</Typography>
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <Stack
+        width={"100%"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={4}
+        padding={4}
+      >
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <Typography fontSize={20}>
+          {isLoaded && isSignedIn && user.fullName}
+        </Typography>
+      </Stack>
+    </Box>
+  );
 
-    if (!isLoaded || !isSignedIn){
-      console.log("HIHIHII")
-      return <ThemeProvider theme={darkTheme}>
-                <AppBar position="static" color={"secondary"}>
-                  <Toolbar>
-                    <Link href={"/"} style={{ flexGrow: 1 }}>
-                      <Typography variant="h6">
-                        AStar Rate my Professor
-                      </Typography>
-                    </Link>
-                    <SignedOut>
-                      <Button color="inherit" href="/sign-in">
-                        Login
-                      </Button>
-                      <Button color="inherit" href="/sign-up">
-                        Sign Up
-                      </Button>
-                    </SignedOut>
-                    <SignedIn>
-                      
-                      <UserButton />
-                    </SignedIn>
-                  </Toolbar>
-              </AppBar>
-              {/* <Drawer
-                    open={open} onClose={toggleDrawer(false)}
-                  >
-                    {EmptyDrawerList}
-              </Drawer> */}
-            </ThemeProvider>;
-    }
-
-    return <ThemeProvider theme={darkTheme}>
+  if (!isLoaded || !isSignedIn) {
+    console.log("HIHIHII");
+    return (
+      <ThemeProvider theme={darkTheme}>
         <AppBar position="static" color={"secondary"}>
           <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ 
-                  mr: 2 
-                }}
-                onClick={toggleDrawer(true)}
-              >
-              <MenuIcon>
-              </MenuIcon>
-            </IconButton>
             <Link href={"/"} style={{ flexGrow: 1 }}>
-              <Typography 
-                variant="h6"
-                className="apply"  
-              >
-                AStar Rate my Professor
-              </Typography>
+              <Typography variant="h6">AStar Rate my Professor</Typography>
             </Link>
             <SignedOut>
               <Button color="inherit" href="/sign-in">
@@ -153,15 +116,61 @@ export default function NavBar(){
               </Button>
             </SignedOut>
             <SignedIn>
-              
               <UserButton />
             </SignedIn>
           </Toolbar>
-      </AppBar>
-      <Drawer
-            open={open} onClose={toggleDrawer(false)}
+        </AppBar>
+        {/* <Drawer
+                    open={open} onClose={toggleDrawer(false)}
+                  >
+                    {EmptyDrawerList}
+              </Drawer> */}
+      </ThemeProvider>
+    );
+  }
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position="static" color={"secondary"}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{
+              mr: 2,
+            }}
+            onClick={toggleDrawer(true)}
           >
-            {DrawerList}
+            <MenuIcon></MenuIcon>
+          </IconButton>
+          <Link href={"/"} style={{ flexGrow: 1 }}>
+            <Typography variant="h6" className="apply">
+              AStar Rate my Professor
+            </Typography>
+          </Link>
+          <SignedOut>
+            <Button color="inherit" href="/sign-in">
+              Login
+            </Button>
+            <Button color="inherit" href="/sign-up">
+              Sign Up
+            </Button>
+          </SignedOut>
+
+          <SignedIn>
+            <Box sx={{ display: "flex", gap: 2 }}>
+              <FlameIcon />
+              <UserButton />
+            </Box>
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
       </Drawer>
-      </ThemeProvider>;
+    </ThemeProvider>
+  );
 }
