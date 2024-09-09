603c050512c42c4c99dbe7811b1697443153956c
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-08 18:06:20 -0700

    added fitness bot

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
 
   const sendPerson = () =>{
     if(isSignedIn){
-      router.push(`/ChatBot`)
+      router.push(`/FitnessBot`)
     }else{
       router.push("/sign-in")
     }
