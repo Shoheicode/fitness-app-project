783ca3096b00427a839cd8ddf4bf4b1ba88b8b25
ShoheiCode <ucodejasonirie@gmail.com>
2025-04-25 15:34:12 -0700

    changed name to make sense

--- app/about/page.js
+++ app/about/page.js
@@ -4,7 +4,7 @@ import NavBar from "@/components/navbar/navbar";
 import { Box, Button, Stack, TextField } from "@mui/material";
 import { useState, Fragment } from "react";
 
-export default function Home() {
+export default function About() {
   return (
     <Box>
         <NavBar />
